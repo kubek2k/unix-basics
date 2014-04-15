1b4f61d08eefc77bd2fa3be056a6fc78071fc820
Jakub Janczak <jakub.janczak@schibsted.pl>
2014-04-15 21:19:28 +0200

    Password management fix

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -12,9 +12,20 @@ define create_users {
     user { $name:
         ensure => "present",
         shell => "/bin/bash",
-        managehome => true,
-        password => "standardpass",
+        managehome => "true",
     }
+
+    exec { 'set password':
+        command => "usermod -p '\$6\$QB2HYPCb\$Rce2CQBnF8CKLPDqLo4uycYGxqhzbOOFckM3UiXkFgvEwkR/vGaIKNFE3XJbwf9UbioNfL4pdPVunA/Y/FjNo0' ${name}",
+        require => User[$name];
+    }
+
+#    file {
+ #       "/home/${name}/textfiles": 
+  #          ensure => "present",
+   #         recurse => "true",
+    #        source => "${settings::confdir}/homedir/textfiles"
+    #}
 }
 create_users { $users: }
 
